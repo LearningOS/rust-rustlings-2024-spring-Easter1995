2ffab487f2f904fd25c0164e77df3423dcc44be7
Easter1995 <1073208733@qq.com>
2024-04-10 12:44:29 +0800

    function completed

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,17 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(number:i32) -> i32 {
+    let ret:i32;
+    if number > 40 {
+        ret = number;
+    } else {
+        ret = number*2;
+    }
+    ret
+}
 
 // Don't modify this function!
 #[test]
